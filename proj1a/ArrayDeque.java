4654ccca1af7cd7eb83febf9c13a5dd03f321073
Feng Zizhe <2836597895@qq.com>
2023-03-17 20:27:47 +0800

    completed proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -5,30 +5,40 @@ public class ArrayDeque<T> {
 	private int size;
 
 	public ArrayDeque() {
-		items = (T[]) new Object[100];
-		head = 0;
+		items = (T[]) new Object[8];
+		head = items.length - 1;
 		tail = 0;
 		size = 0;
 	}
 
+	/** Resize the ArrayDeque */
+	private void resize(int capacity) {
+		T[] target = (T[]) new Object[capacity];
+		System.arraycopy(items, 0, target, 0, tail);
+		int length = items.length - 1 - head;
+		System.arraycopy(items, head + 1, target, capacity - length, length);
+		head = capacity - length - 1;
+		items = target;
+	}
+
 	/** Adds the item to the front of the list. */
 	public void addFirst(T item) {
-		if (size == 0) {
-			tail = tail == items.length - 1 ? 0 : tail + 1;
+		if (head - 1 == nextLast) {
+			resize(items.length * 2);
 		}
 		items[head] = item;
 		size = size + 1;
-		head = head == 0 ? items.length - 1 : head - 1;
+		head = head - 1;
 	}
 
 	/** Adds the item to the end of the list. */
 	public void addLast(T item) {
-		if (size == 0) {
-			head = head == 0 ? items.length - 1 : head - 1;
+		if (head - 1 == nextLast) {
+			resize(items.length * 2);
 		}
 		items[tail] = item;
 		size = size + 1;
-		tail = tail == items.length - 1 ? 0 : tail + 1;
+		tail = tail + 1;
 	}
 
 	/** Returns the boolean value whether the list is empty. */
@@ -43,32 +53,38 @@ public class ArrayDeque<T> {
 
 	/** Remove the first item of the list and returns. */
 	public T removeFirst() {
-		if (size == 0) {
+		if (head == items.length - 1) {
 			return null;
 		}
 		size = size - 1;
-		head = head == items.length - 1 ? 0 : head + 1;
+		head = head + 1;
 		T item = items[head];
+		if (items.length >= 16 && size < items.length/4) {
+			resize(items.length/2);
+		}
 		return item;
 	}
 
 	/** Remove the last item of the list and returns. */
 	public T removeLast() {
-		if (size == 0) {
+		if (tail == 0) {
 			return null;
 		}
-		size = size - 1;
-		tail = tail == 0 ? items.length - 1 : tail - 1;
+		tail = tail - 1;
 		T item = items[tail];
-		return item;
+		items[tail] = null;
+		size = size - 1;
+		if (items.length >= 16 && size < items.length/4) {
+			resize(items.length/2);
+		}
 	}
 
 	/** Returns the item of the ith of the list. */
 	public T get(int index) {
-		if (head + index + 1 > items.length - 1) {
-			return items[head + index - items.length + 1];
+		if (index >= size) {
+			return null;
 		}
-		return items[head + index + 1];
+		return items[(head + 1 + index) % items.length];
 	}
 
 	/** Prints the items in the deque from first to last, separated by a space. */
